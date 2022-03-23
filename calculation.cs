2ce8b9c3ec49d7b83d4b54e8fdd1cbc44be4ec3f
Shyubha <86038837+Shyubha@users.noreply.github.com>
2022-03-23 18:59:38 +0530

    Add project files.

---
+++ calculation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console
+{
+    public delegate int MyDelegate(int n1, int n2);
+
+    public delegate string MyStrDel(string name);
+
+    public class calculation
+    {
+        public int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        public int Subtractio(int a, int b)
+        {
+            return a - b;
+        }
+        public int Multiplay(int x, int y)
+        {
+            return x * y;
+        }
+
+    }
+    public class Test
+    {
+        public string AcceptName(string name)
+        {
+            return name.ToUpper();
+        }
+        public string AcceptNameL(string nameL)
+        {
+            return nameL.ToLower();
+        }
+    }
+
+}
+    
